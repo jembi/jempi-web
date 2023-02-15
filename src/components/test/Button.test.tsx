46d7136867cd8d556d5b8042b301608dc960ac03
Lumiere-Lodi <lumiere@symbionix.co>
2023-02-16 01:48:44 +0200

    Unit Testing for button component

---
+++ src/components/test/Button.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+import Button from '../shared/Button'
+
+test('Render Button Component', () => {
+  render(<Button>Search</Button>)
+  const linkElement = screen.getAllByText(/Search/i)
+  expect(linkElement[0]).toBeInTheDocument()
+})
+
+test('Add full-width className when isFullWidth is true', () => {
+  const { container } = render(<Button isFullWidth={true}>Search</Button>)
+  expect(container.firstChild).toHaveClass('MuiButtonBase-root')
+})
+
+test('Show Circular progress icon when isLoading is true', () => {
+  render(<Button isLoading={true}>Test</Button>)
+
+  const { container } = render(<Button isLoading={true}>Search</Button>)
+  console.log(container.classList.values)
+  expect(
+    container.getElementsByClassName('MuiCircularProgress-circle')
+  ).toBeTruthy()
+})
