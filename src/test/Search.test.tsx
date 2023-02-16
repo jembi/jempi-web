55ac509d2f750d3ef46cbc6ef64f747b047f3c8c
Lumiere-Lodi <lumiere@symbionix.co>
2023-02-16 15:11:53 +0200

    Simple search unit test

---
+++ src/test/Search.test.tsx
@@ -0,0 +1,90 @@
+import { ReactLocation, Route, Router } from '@tanstack/react-location'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { act, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { AppConfigProvider } from '../hooks/useAppConfig'
+import CustomSearch from '../components/customSearch/CustomSearch'
+import SimpleSearch from '../components/search/SimpleSearch'
+import SearchResult from '../components/searchResult/SearchResult'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {}
+  }
+})
+const location = new ReactLocation()
+const routes: Route[] = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'search',
+        children: [
+          {
+            path: 'simple',
+            element: <SimpleSearch />
+          },
+          {
+            path: 'custom',
+            element: <CustomSearch />
+          }
+        ]
+      },
+      {
+        path: 'search-results',
+        children: [
+          {
+            path: 'golden',
+            element: (
+              <SearchResult isGoldenRecord={true} title="Golden Records Only" />
+            )
+          },
+          {
+            path: 'patient',
+            element: (
+              <SearchResult
+                isGoldenRecord={false}
+                title="Patient Records Only"
+              />
+            )
+          }
+        ]
+      }
+    ]
+  }
+]
+
+const renderSimpleSearch = () => {
+  act(() => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router location={location} routes={routes}>
+          <AppConfigProvider>
+            <SimpleSearch />
+          </AppConfigProvider>
+        </Router>
+      </QueryClientProvider>
+    )
+  })
+}
+
+test('Custom Search button exists when simple search component is rendered', async () => {
+  await renderSimpleSearch()
+
+  const linkElement = await screen.findAllByText(/Custom Search/i)
+  expect(linkElement[0]).toBeInTheDocument()
+})
+
+test('Simple Search button exist when we navigate to custom search', async () => {
+  await renderSimpleSearch()
+
+  const customSearchButton = screen.getAllByRole('link', {
+    name: /Custom Search/i
+  })[0]
+
+  act(() => {
+    userEvent.click(customSearchButton)
+  })
+
+  expect(await screen.findByText(/Simple Search/i)).toBeInTheDocument()
+})
---
+++ src/test/ToggleButtons.test.tsx
@@ -0,0 +1,34 @@
+import { SxProps, Theme } from '@mui/material'
+import { render, screen } from '@testing-library/react'
+import ToggleButtons from '../components/search/ToggleButtons'
+
+const options = [
+  { value: 0, label: '0' },
+  { value: 1, label: '1' },
+  { value: 2, label: '2' },
+  { value: 3, label: '3' }
+]
+
+const sx: SxProps<Theme> = {
+  width: '129px',
+  height: '42px',
+  borderColor: theme => theme.palette.primary.main,
+  color: theme => theme.palette.primary.main,
+  '&.Mui-selected, &.Mui-selected:hover': {
+    color: 'white',
+    backgroundColor: theme => theme.palette.primary.main
+  }
+}
+test('Have content 1 when passed as an option label', () => {
+  const { container } = render(
+    <ToggleButtons selectedButton="1" options={options} sx={sx} />
+  )
+  expect(container.firstChild).toHaveTextContent('1')
+})
+
+test('Have option one when passed as the initial selected value', () => {
+  render(<ToggleButtons selectedButton="1" options={options} sx={sx} />)
+
+  const linkElement = screen.getAllByText('1')
+  expect(linkElement[0]).toHaveTextContent('1')
+})
