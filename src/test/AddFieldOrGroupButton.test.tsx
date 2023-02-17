05440a7c4995266f73e5e68b252d5924dc961e26
Lumiere-Lodi <lumiere@symbionix.co>
2023-02-17 16:12:18 +0200

    apply DRY

--- src/test/AddFieldOrGroupButton.test.tsx
+++ src/test/AddFieldOrGroupButton.test.tsx
@@ -1,4 +1,4 @@
-import { act, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import AddFieldOrGroupButton from '../components/customSearch/AddFieldOrGroupButton'
 
