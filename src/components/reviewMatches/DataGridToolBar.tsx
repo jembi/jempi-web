1d05e3d77f119e660bfa34b42da2b3bd4644f3ba
Ilyes BENAMARA <ilyesb2@gmail.com>
2023-03-02 17:17:23 +0100

    feat: add ability to filter with multiple columns

--- src/components/reviewMatches/DataGridToolBar.tsx
+++ src/components/reviewMatches/DataGridToolBar.tsx
@@ -6,7 +6,15 @@ const DataGridToolbar: FC = () => {
     <GridToolbarContainer
       sx={{ p: 2, display: 'flex', gap: 2, alignItems: 'baseLine' }}
     >
-      <GridToolbarQuickFilter />
+      <GridToolbarQuickFilter
+        quickFilterParser={searchInput =>
+          searchInput.split(',').map(value => value.trim())
+        }
+        quickFilterFormatter={quickFilterValues =>
+          quickFilterValues ? quickFilterValues.join(', ') : ''
+        }
+        debounceMs={200}
+      />
     </GridToolbarContainer>
   )
 }
